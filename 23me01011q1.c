2a688df4e29ccc5740e753b7e899da4d10ce3a08
yellownikki96 <149192495+yellownikki96@users.noreply.github.com>
2023-11-10 02:39:13 +0530

    23me01011q1.c

--- 23me01011q1.c
+++ 23me01011q1.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-void swap(int *num1, int *num2);
+void swap(int *num1, int *num2)
+{
+    int temp;
+    temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+}
 
 int main()
 {
@@ -21,10 +27,4 @@ int main()
     return 0;
 }
 
-void swap(int *num1, int *num2)
-{
-    int temp;
-    temp = *num1;
-    *num1 = *num2;
-    *num2 = temp;
-}
+
